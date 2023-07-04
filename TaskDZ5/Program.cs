a8450f54c0e166fa2dde024ff68d2ec33b9d4f0f
nikitasergun <137383135+nikitasergun@users.noreply.github.com>
2023-07-05 04:01:40 +0700

    Add files via upload

---
+++ TaskDZ5/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//Пример
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int getSecondNumber(int num)
+{
+    int number = System.Math.Abs(num / 10 % 10);
+    return number;
+}
+Console.WriteLine(getSecondNumber(456));
+Console.WriteLine("----------------------");
+Console.WriteLine(getSecondNumber(782));
+Console.WriteLine("----------------------");
+Console.WriteLine(getSecondNumber(918));
+Console.WriteLine("----------------------");
---
+++ TaskDZ6/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine();
+void getThirdNumber(int num)
+{
+    int number = 0;
+    int tmp = num; // только для красивого вывода
+
+    if (num < 100)
+        Console.WriteLine($"{num} двухзначное число");
+    else
+    {
+        while (num > 100)
+        {
+            number = num % 10;
+            num = System.Math.Abs(num / 10);
+        }
+        Console.WriteLine($"Третья цифра числа {tmp} = {number}");
+    }
+}
+getThirdNumber(645);
+getThirdNumber(78);
+getThirdNumber(32679);
+Console.WriteLine("\n----------------------");
