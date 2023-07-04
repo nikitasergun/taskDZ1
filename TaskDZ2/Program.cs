2da4bdc55f5b9928bbb3ba180802828ace254043
nikitasergun <137383135+nikitasergun@users.noreply.github.com>
2023-07-05 03:03:08 +0700

    Add files via upload

---
+++ TaskDZ2/Program.cs
@@ -0,0 +1,16 @@
+﻿//-Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+var number = Convert.ToInt32(Console.ReadLine());
+    var number2 = Convert.ToInt32(Console.ReadLine());
+    var number3 = Convert.ToInt32(Console.ReadLine());
+                if (number > number2 && number > number3)
+                    Console.WriteLine("Nubmer Max = " + number); 
+                else if (number2 > number3)
+                    Console.WriteLine("Number Max = " + number2); 
+                else
+                    Console.WriteLine("Number Max = " + number3); 
+            
+            Console.ReadLine();
---
+++ TaskDZ3/Program.cs
@@ -0,0 +1,31 @@
+﻿// - Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4->да
+//- 3->нет
+//7->нет
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;                             
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine()); 
+ 
+            if (a % 2 == 0){Console.WriteLine("четное число");}
+            else
+            {Console.WriteLine("нечетное"); }
+ 
+ 
+ 
+            Console.ReadKey();
+        }
+    }
+}
